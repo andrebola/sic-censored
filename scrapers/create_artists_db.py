8878578de771e3bfb5832154b3185483bff5bb3a
Sylvain Le Groux <slegroux@ccrma.stanford.edu>
2020-11-11 16:11:25 -0800

    automatically generate data from region/country list

--- scrapers/create_artists_db.py
+++ scrapers/create_artists_db.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python
-from freemuse import get_artists_from_freemuse
+
+from freemuse import FreemuseParser
+import pickle
 from IPython import embed
+import time
+import multiprocessing
 
 # TODO: add other countries
 
+map_tiny = {
+    'europe': ['spain', 'italy']
+}
+
 map_small = {
-    'africa': ['angola'],
+    'africa': ['cameroon'],
     'europe': ['spain','france']
 }
 
@@ -17,10 +25,39 @@ map = {
     'middle-east-north-africa': ['algeria', 'bahrain', 'egypt', 'iran']
 }
 
-artists = {}
+def get_artists(map):
+    artists = {}
+    inputs = set()
+    for region, countries in map.items():
+        for country in countries:
+            inputs.add((region,country))
+            # artists.update(parser.get_artists(region, country))
+    tic = time.perf_counter()
+    with multiprocessing.Pool(multiprocessing.cpu_count()) as p:
+        res = p.starmap(get_artists, inputs)
+    toc = time.perf_counter()
+    print("time: {:0.2f}".format(toc-tic))
+    return(res)
 
-for region, countries in map_small.items():
-    for country in countries:
-         artists.update(get_artists_from_freemuse(region, country))
 
-embed()
+if __name__ == "__main__":
+    parser = FreemuseParser()
+    artists = {}
+    inputs = set()
+    for region, countries in map_tiny.items():
+        for country in countries:
+            inputs.add((region,country))
+            # artists.update(parser.get_artists(region, country))
+    tic = time.perf_counter()
+    with multiprocessing.Pool(multiprocessing.cpu_count()) as p:
+        res = p.starmap(parser.get_artists, inputs)
+    toc = time.perf_counter()
+    print("time: {:0.2f}".format(toc-tic))
+    print(res)
+    embed()
+    # try:
+    #     fd = open(MAP + '.pkl','wb')
+    #     pickle.dump(artists, fd)
+    #     fd.close()
+    # except Exception as e:
+    #     print(e)
